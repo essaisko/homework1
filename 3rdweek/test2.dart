b61a29d1d9635af60c21c34d4ff40bfd3ac4fc85
essaisko <essaisko@gmail.com>
2025-03-17 16:09:03 +0900

    입력 이름 유효성 검사 정규 표현식 수정 및 오류 메시지 개선

--- 3rdweek/test2.dart
+++ 3rdweek/test2.dart
@@ -192,9 +192,9 @@ String getCharacterName() {
     String? name = stdin.readLineSync();
     if (name != null &&
         name.isNotEmpty &&
-        RegExp(r'^[a-zA-Z가-힣]+\$').hasMatch(name)) {
+        RegExp(r'^[a-zA-Z가-힣]+$').hasMatch(name)) {
       return name;
     }
-    print("⚠️ 올바른 이름을 입력하세요! (한글, 영문 대소문자만 가능)");
+    print("이름은 한글 또는 영문만 가능합니다. 다시 입력해주세요.");
   }
 }
